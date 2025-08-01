2d70c6339f3e17dc557d7584bdc4081617078f1c
Rishabh Saraswat <122351049+rishabhsaraswat-cs22@users.noreply.github.com>
2025-08-01 11:18:42 +0530

    final

--- Day3/q5.java
+++ Day3/q5.java
@@ -5,7 +5,14 @@ public class q5 {
         int n = sc.nextInt();
         help(n);
     }
-    public static void help(int n){
-        
+    public static void help(int n) {
+        if (n == 1) {
+            System.out.print("1 ");
+            return;
+        }
+    
+        help(n - 1);
+        System.out.print(n + " ");
+        help(n - 1);
     }
 }
